12988b3eb789360d1dfd749af8364f1534ebd825
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-04-02 11:50:58 +0300

    nav bar initiated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
+
+
+
 import type { NextPage } from 'next';
+import BaseLayout from "../components/layout/BaseLayout";
 
 const Home: NextPage = () => {
 
   return (
-  <>
-  </>
+  <BaseLayout>
+  <div>
+  	Hello World
+  </div>
+  </BaseLayout>
   )
 }
 

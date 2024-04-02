cf71dcfe4fac2044e25389f71627eaa5ba949b06
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-04-02 11:31:07 +0300

    second commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../globals.css';
+import type { AppProps } from 'next/app'
+import { Navbar } from 'components';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    	<Navbar />
+        <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We're here to provide you with the best information and services.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
+
